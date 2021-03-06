c8231418b4a4acf7d2f59c34bb6b529031a09019
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2021-03-06 14:31:34 +0000

    Run spin animation on click

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,26 @@ leftBtn.textContent = leftText;
 rightBtn.textContent = rightText;
 title.innerText = leftText + " or " + rightText + "?";
 console.log("right btn", rightBtn);
+
+function clickImg(){
+    const element = document.getElementById("main-img");
+
+    //reset transition by removing class and re-adding
+    element.addEventListener("click", function(e){
+        e.preventDefault();
+
+        element.classList.remove("btn-img-anim");
+        void element.offsetWidth;
+
+        element.classList.add("btn-img-anim");
+    });
+}
+
+function clickLeft(){
+    console.log("LEFT");
+
+}
+
+function clickRight(){
+    console.log("Right");
+}
